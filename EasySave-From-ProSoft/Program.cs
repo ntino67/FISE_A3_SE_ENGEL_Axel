a2595c80c7c3447f57822ceb2f1327218d88d540
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-05 14:41:02 +0200

    init: Project intiatlisation

---
+++ EasySave-From-ProSoft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasySave_From_ProSoft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
