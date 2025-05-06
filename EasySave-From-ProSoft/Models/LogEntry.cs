3866bfcb545bc58fd83900df25812559df1dd045
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-06 17:21:14 +0200

    LogEntry POCOs

---
+++ EasySave-From-ProSoft/Models/LogEntry.cs
@@ -0,0 +1,9 @@
+public class LogEntry
+{
+    public DateTime timestamp { get; set; }
+    public string jobName { set; get; }
+    public string sourcePath { set; get; }
+    public string targetPath { set; get; }
+    public long fileSize { set; get; }
+    public long transferSize { set; get; }
+}
