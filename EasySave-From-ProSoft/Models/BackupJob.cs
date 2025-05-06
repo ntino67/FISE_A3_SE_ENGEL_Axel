b6d7079350c67fd9620bfcdf263b640ce54a2ed0
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-06 17:20:39 +0200

    Backup Log Poco

---
+++ EasySave-From-ProSoft/Models/BackupJob.cs
@@ -0,0 +1,7 @@
+public class BackupJob
+{
+    public string name { get; set; }
+    public string source { get; set; }
+    public string target { get; set; }
+    public BackupType Type { get; set; }
+}
