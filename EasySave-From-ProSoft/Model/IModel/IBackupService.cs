8f590c9bf3918dad0001c5dab524287fd48f2b6c
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-09 11:22:31 +0200

    Added interfaces

--- EasySave-From-ProSoft/Model/IModel/IBackupService.cs
+++ EasySave-From-ProSoft/Model/IModel/IBackupService.cs
@@ -1,1 +1,11 @@
-// hello
+namespace EasySave.Interfaces
+{
+    public interface IBackupService
+    {
+        public BackupResult ExecuteBackupJob(string idName);
+        public List<BackupResult> ExecuteAllBackupJobs();
+        public void AddBackupJob(BackupJob job);
+        public void DeleteBackuJob(string idName);
+        public List<BackupJob> GetAllJobs();
+    }
+}
--- EasySave-From-ProSoft/Model/IModel/IConfigurationManager.cs
+++ EasySave-From-ProSoft/Model/IModel/IConfigurationManager.cs
@@ -1,1 +1,10 @@
-// hello
+namespace EasySave.Interfaces
+{
+    public interface IConfigurationManager
+    {
+        public List<BackupJob> LoadJobConfig();
+        public void saveJobConfigs(List<BackupJob> config);
+        public AppConfiguration LoadAppConfig();
+        public void saveAppConfig(AppConfiguration config);
+    }
+}
