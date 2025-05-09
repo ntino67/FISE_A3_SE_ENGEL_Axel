9b43becf245f583cf08806ef41ff951c9360acac
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-09 12:26:19 +0200

    Added logger use, defined a robust instance, added .idea to .gitignore

--- EasySave-From-ProSoft/Model/ImplementIModel/Logger.cs
+++ EasySave-From-ProSoft/Model/ImplementIModel/Logger.cs
@@ -1,47 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
 using EasySave_From_ProSoft.Model.IModel;
-using EasySave_From_ProSoft.Model.ImplementIModel;
+using EasySave.Model.ImplementIModel;
 
 namespace EasySave_From_ProSoft.Model.ImplementIModel
 {
     public sealed class Logger : ILogger
     {
-        private static readonly object LockObj = new{};
-        private static Logger _instance { get; set; }
-
-        public string logFilePath { get; set; } = "logs.json";
-
         private Logger() { }
+
+        private static Logger _instance { get; set; }
+        public string logFilePath { get; set; }
 
         public static Logger GetInstance()
         {
-            lock (LockObj)
+            if (_instance == null)
             {
-                _instance ??= new Logger();
-                return _instance;
+                _instance = new Logger();
             }
+            return _instance;
         }
 
         public void Log(LogEntry entry)
         {
-            List<LogEntry> logList = new List<LogEntry>;
-
-            if (File.Exists(logFilePath))
-            {
-                string existingJson = File.ReadAllText(logFilePath);
-                if (!string.IsNullOrWhiteSpace(existingJson))
-                {
-                    logList = JsonSerializer.Deserializer<List<LogEntry>>(existingJson) ?? new List<LogEntry>();
-                }
-            }
-
-            logList.Add(LogEntry);
-
-            string updatedJson = JsonSerializer.Serialize(logList, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(LogFilePath, updatedJson);
+            throw new Exception("not implemented yet");
         }
         
         public List<LogEntry> LoadLog(DateTime date)
