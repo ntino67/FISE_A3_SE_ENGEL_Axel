e2caa7b0546d3627ee85a5788a262a6f85b3ac79
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-14 01:05:16 +0200

    Implement INotify for BackupJob too

--- EasySave-From-ProSoft/Model/BackupJob.cs
+++ EasySave-From-ProSoft/Model/BackupJob.cs
@@ -1,33 +1,115 @@
 ﻿using System;
 using System.Text.Json.Serialization;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using EasySave_From_ProSoft.Utils;
 
 namespace EasySave_From_ProSoft.Model
 {
-    public class BackupJob
+    public class BackupJob : INotifyPropertychanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _name;
+
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _sourceDirectory;
 
         [JsonPropertyName("sourceDirectory")]
-        public string SourceDirectory { get; set; }
+        public string SourceDirectory
+        {
+            get => _sourceDirectory;
+            set
+            {
+                if (_sourceDirectory != value)
+                {
+                    _sourceDirectory = value;
+                }
+            }
+        }
+
+        private string _targetDirectory;
 
         [JsonPropertyName("targetDirectory")]
-        public string TargetDirectory { get; set; }
+        public string TargetDirectory
+        {
+            get => _targetDirectory;
+            set
+            {
+                if (_targetDirectorya != value)
+                {
+                    _targetDirectory = value;
+                }
+            }
+        }
+
+        private BackupType _type;
 
         [JsonPropertyName("type")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public BackupType Type { get; set; }
+        public BackupType Type
+        {
+            get => _type;
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                }
+            }
+        }
 
         [JsonPropertyName("id")]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
+        private DateTime? _lastRunTime;
+
         [JsonPropertyName("lastRunTime")]
-        public DateTime? LastRunTime { get; set; }
+        public DateTime? LastRunTime
+        {
+            get => _lastRunTime;
+            set
+            {
+                if (_lastRunTime != value)
+                {
+                    _lastRunTime = value;
+                }
+            }
+        }
+
+        private JobStatus _status = JobStatus.Ready;
 
         [JsonPropertyName("state")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public JobStatus Status { get; set; } = JobStatus.Ready;
+        public JobStatus Status
+        {
+            get => _status;
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                }
+            }
+        }
 
         // Méthode de validation du job
         public bool IsValid()
