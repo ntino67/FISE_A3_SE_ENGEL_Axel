6926e92965ac01d24fd4f14cac2068b2ed3bea72
AxeWizard677 <axel.engel@viacesi.fr>
2025-05-16 15:30:39 +0200

    Worked on WPF UI

--- EasySave-2.0-from-ProSoft/MainWindow.xaml.cs
+++ EasySave-2.0-from-ProSoft/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,33 @@ namespace EasySave_2._0_from_ProSoft
         public MainWindow()
         {
             InitializeComponent();
+
+            // Exemple de données
+            List<JobItem> JobsList = new List<JobItem>
+            {
+                new JobItem { Name = "Backup 1", IsChecked = true, IsActive = false },
+                new JobItem { Name = "Backup 2", IsChecked = false, IsActive = true }
+            };
+                for (int i = 2; i < 100; i++)
+                {
+                    JobsList.Add(new JobItem { Name = "Backup " + (i + 1), IsChecked = false, IsActive = false });
+                }
+            JobList.ItemsSource = JobsList;
         }
+
+        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
     }
+
+}
+
+public class JobItem
+{
+    public string Name { get; set; }
+    public bool IsChecked { get; set; }
+    public bool IsActive { get; set; }
 }
