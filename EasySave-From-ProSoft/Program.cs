00ffe83ba9eeac4b09c9f1b31643b98e48d6d8ed
JoMonKiRi <johan.rinaldi70@gmail.com>
2025-05-16 10:24:58 +0200

    Rework environment

--- EasySave-From-ProSoft/Program.cs
+++ EasySave-From-ProSoft/Program.cs
@@ -1,16 +1,10 @@
-﻿namespace EasySave_From_ProSoft
-{
-    using EasySave_From_ProSoft.View;
-    using EasySave_From_ProSoft.ViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-    public static class Program
+namespace EasySave_From_ProSoft
+{
+    internal class Program
     {
-        public static void Main(string[] args)
-        {
-            ViewModelLocator.Initialize();
-            var view = new ConsoleView();
-            var flow = new ConsoleFlow(view, ViewModelLocator.GetJobViewModel());
-            flow.Run();
-        }
     }
 }
