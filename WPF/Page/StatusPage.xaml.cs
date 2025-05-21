7c7ff2a15611f94d3a4b2a0181c2d6ae68f0c315
ntino67 <matteo.heidelberger@viacesi.fr>
2025-05-21 18:37:38 +0200

    Fixed the start, fixed the jobs not in the jobs in the view model

--- WPF/Page/StatusPage.xaml.cs
+++ WPF/Page/StatusPage.xaml.cs
@@ -1,22 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace EasySave_2._0_from_ProSoft
+﻿namespace WPF
 {
-    /// <summary>
-    /// Logique d'interaction pour StatusPage.xaml
-    /// </summary>
-    public partial class StatusPage : Page
+    public partial class StatusPage : System.Windows.Controls.Page
     {
         public StatusPage()
         {
