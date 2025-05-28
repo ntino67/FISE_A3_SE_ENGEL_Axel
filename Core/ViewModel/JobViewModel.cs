5b589308305d22b01735ccb9abc7966f8bf76c36
JoMonKiRi <johan.rinaldi70@gmail.com>
2025-05-28 14:05:30 +0200

    Implement Single-instance application in app.xaml.cs

--- Core/ViewModel/JobViewModel.cs
+++ Core/ViewModel/JobViewModel.cs
@@ -521,6 +521,20 @@ namespace Core.ViewModel
             OnPropertyChanged(nameof(EncryptionStatus));
         }
         
+        public bool IsActive
+        {
+            get => CurrentJob?.IsActive ?? false;
+            set
+            {
+                if (CurrentJob != null && CurrentJob.IsActive != value)
+                {
+                    CurrentJob.IsActive = value;
+                    _jobManager.UpdateBackupJob(CurrentJob);
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
         private void RefreshJobBindings()
         {
             OnPropertyChanged(nameof(SourceDirectoryLabel));
