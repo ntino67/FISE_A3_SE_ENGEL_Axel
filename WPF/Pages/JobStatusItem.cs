39e499186bffa0f7ddfb04087300528bd7795fbb
JoMonKiRi <johan.rinaldi70@gmail.com>
2025-05-30 19:30:05 +0200

    Finnaly finish the backup status interface + job storage are implement in jobviewmodel and not in mainwindow | Going for a crazy rebase!

---
+++ WPF/Pages/JobStatusItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WPF.Pages
+{
+    internal class JobStatusItem
+    {
+        public string Status { get; internal set; }
+        public int Progress { get; internal set; }
+        public DateTime EndTime { get; internal set; }
+        public string JobName { get; internal set; }
+        public DateTime StartTime { get; internal set; }
+    }
+}
